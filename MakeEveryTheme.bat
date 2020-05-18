a6c2e5ef18c927c5391702ea0f81be322715998c
Migushthe2nd <Migushthe2nd@users.noreply.github.com>
2020-05-18 16:26:44 +0200

    Push changes

--- MakeEveryTheme.bat
+++ MakeEveryTheme.bat
@@ -1,8 +1,11 @@
 @echo off
 Setlocal EnableDelayedExpansion
 rem Set important variables
+	rem Set current folder
+		Set "currDir=%cd%"
 	rem Set release folder
-		Set /p ThemeName=Enter the name of the theme (eg. Careful Layout ENX v1): 
+		ECHO.
+		Set /p ThemeName=Enter the name of the theme (eg. 'My Theme'): 
 		Set "ReleaseFolder=Finished\%ThemeName%"
 	rem Set the SwitchThemeInjector directory
 		Set "StiEXE=K:\NSW\Themes\Very nice theme\tools\SwitchThemeInjector\Release4.0\SwitchThemes.exe"
@@ -22,7 +25,8 @@ ECHO     Now Drag and drop the layout json on this window and press enter
 ECHO ........................................................................
 ECHO.
 SET /P jsonfile=
-IF NOT EXIST %jsonfile% GOTO NotExist1
+IF NOT DEFINED jsonfile GOTO NotExist1
+IF NOT EXIST "%jsonfile%" GOTO NotExist1
 CALL :dequote jsonfile
 :DeQuote
 for /f "delims=" %%A in ('echo %%%1%%') do set %1=%%~A
@@ -40,11 +44,11 @@ GOTO MENU0.5
 :MENU0.55
 clear
 	ECHO.
-	ECHO ......................................................
+	ECHO ...............................................
 	ECHO     Do you want to use a custom DDS folder? 
-	ECHO ......................................................
+	ECHO ...............................................
 	ECHO.
-	ECHO (Leaving this empty will use the default directory)
+	ECHO (Leaving this empty will use the default directory: './backgrounds')
 	ECHO.
 	
 	SET /P DDSFolder=
@@ -69,7 +73,7 @@ if EXIST "%OverlayPreview%" GOTO MENU2
 	ECHO     Would you like to create the folder now?
 	ECHO .....................................................................
 	ECHO.
-	ECHO 1 - Yes
+	ECHO 1 - Yes (make sure to put the overlays after this step in the folder!)
 	ECHO 2 - No (will still create the NXThemes)
 	ECHO.
 	
@@ -104,9 +108,9 @@ if EXIST "%OverlayPreview%" GOTO MENU2
 :MENU2
 clear
 ECHO.
-ECHO ....................................
+ECHO ..................................
 ECHO     What would You like to do?
-ECHO ....................................
+ECHO ..................................
 ECHO.
 ECHO 1 - Create Homemenu theme
 ECHO 2 - Create lockscreen theme
@@ -122,25 +126,25 @@ IF %S2%==1 (
 	set menu=home
 	GOTO MENU2.5
 )
-IF %S1%==2 (
+IF %S2%==2 (
 	set menu=lock
-	GOTO MENU3
+	GOTO All
 )
-IF %S1%==3 (
+IF %S2%==3 (
 	set menu=apps
-	GOTO MENU3
+	GOTO All
 )
-IF %S1%==4 (
+IF %S2%==4 (
 	set menu=set
-	GOTO MENU3
+	GOTO All
 )
-IF %S1%==5 (
+IF %S2%==5 (
 	set menu=user
-	GOTO MENU3
+	GOTO All
 )
-IF %S1%==6 (
+IF %S2%==6 (
 	set menu=news
-	GOTO MENU3
+	GOTO All
 )
 clear
 ECHO That was an error.
@@ -156,7 +160,7 @@ ECHO     If you have a custom common json drop it here as well, otherwise leave 
 ECHO .........................................................................................
 ECHO.
 SET /P jsoncommonfile=
-IF [%jsoncommonfile%] == [] GOTO MENU3
+IF [%jsoncommonfile%] == [] GOTO All
 IF NOT EXIST %jsoncommonfile% GOTO NotExist2
 CALL :dequote jsoncommonfile
 :DeQuote
@@ -197,6 +201,9 @@ REM GOTO MENU3
 REM pause
 
 :All
+clear
+
+IF NOT EXIST "%ReleaseFolder%" GOTO Create
 
 ECHO.
 ECHO .........................................
@@ -220,6 +227,7 @@ clear
 GOTO All
 
 :Create
+cd %currDir%
 ECHO Creating themes with:
 ECHO %jsonfile% 
 for %%a in ("%DDSFolder%\*.dds") do (
@@ -234,9 +242,10 @@ ECHO -------------------------------------------
 	rem Create preview images
 		for %%d in ("%OverlayPreview%\*.png") do (
 			IF [%%d] == [] GOTO EOF
+			set "montageimages="
 			for %%a in ("%DDSFolder%\*.dds") do (
 				ECHO -------------------------------------------
-				ECHO Creating preview for '%ThemeName%' with '%%~nxa'
+				ECHO Creating preview '%%~nd' for '%ThemeName%' with '%%~nxa'
 				if NOT EXIST "%ReleaseFolder%\PreviewImages\%%~nd" mkdir "%ReleaseFolder%\PreviewImages\%%~nd"
 				rem SKIP MAGENTA PREVIEW GENERATION
 					rem if NOT "%%a"=="%%a:MAGENTA=" GOTO SkipPreview
@@ -246,11 +255,12 @@ ECHO -------------------------------------------
 			ECHO.
 			ECHO Making montage of all images...
 			magick montage -geometry +0+0 -label %%t -pointsize 40 -quality 80%% !montageimages! "%ReleaseFolder%\PreviewImages\%%~nd-combined.jpg"
-			ECHO.
-			ECHO Creating zip with all NXThemes and '%ThemeName%-combined.jpg'...
-			cd "%ReleaseFolder%"
-			%Sevenz% a -tzip "%ThemeName% - NXThemes" "*.nxtheme" "PreviewImages\*.jpg"
 		)
+		ECHO.
+		ECHO Creating zip with all NXThemes and '%ThemeName%-combined.jpg'...
+		cd "%ReleaseFolder%"
+		%Sevenz% a -tzip "%ThemeName% - NXThemes" "*.nxtheme" "PreviewImages\*.jpg"
+
 		ECHO -------------------------------------------
 		ECHO.
 		ECHO.
@@ -260,9 +270,9 @@ pause
 clear
 
 ECHO.
-ECHO .............................
-ECHO     Run this round again?
-ECHO .............................
+ECHO ...........................................
+ECHO     Run this exact configuration again?
+ECHO ...........................................
 ECHO.
 ECHO 1 - Yes
 ECHO 2 - No
@@ -270,7 +280,7 @@ ECHO.
 
 SET /P S5=Select a number, then press ENTER: 
 clear
-IF %S4%==1 GOTO Create
+IF %S5%==1 GOTO Create
 clear
 
 :EOF
